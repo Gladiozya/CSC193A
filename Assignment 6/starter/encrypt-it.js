dd4b8cf035a488c05da4c21e96dd8452f37b78b3
Gladiozya <112456676+Gladiozya@users.noreply.github.com>
2024-10-15 00:06:36 -0700

    Assignment 6

--- Assignment 6/starter/encrypt-it.js
+++ Assignment 6/starter/encrypt-it.js
@@ -1,6 +1,7 @@
 /*
  * Starter file 
  */
+
 (function() {
   "use strict";
 
@@ -24,3 +25,41 @@
   // you shouldn't write an entire Java program in the main method).
 
 })();
+
+function handleClick(){
+  console.log("Button clicked!");
+}
+
+function handleReset(){
+  let clearingText=document.getElementById("input-text").innerHTML="";
+  document.getElementById("input-text").value=clearingText;
+}
+
+
+function handleEncrypt(){
+  let message=document.getElementById("input-text").value;
+
+  message=shiftCipher(message);
+  document.getElementById("result").innerHTML=message;
+}
+/**
+ * Returns an encrypted version of the given text, where
+ * each letter is shifted alphabetically ahead by 1 letter,
+ * and 'z' is shifted to 'a' (creating an alphabetical cycle).
+ */
+function shiftCipher(text) {
+  text = text.toLowerCase();
+  let result = "";
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] < 'a' || text[i] > 'z') {
+      result += text[i];
+    } else if (text[i] == 'z') {
+      result += 'a';
+    } else { // letter is between 'a' and 'y'
+      let letter = text.charCodeAt(i);
+      let resultLetter = String.fromCharCode(letter + 1);
+      result += resultLetter;
+    }
+  }
+  return result;
+}
